dcc1fc2d3b6d7d6af7cdb1256fa7e0289c10e197
Qasim Yusofi <qasem.yousifi20@gmail.com>
2025-05-06 21:49:07 +0330

    product page and rate the product connected to DB

---
+++ productPage/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, get_object_or_404,redirect
+from mainPage.models import Product
+from django.http import JsonResponse
+
+def productPage(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'product-page.html', {'product': product})
+
+def rate_product(request, id):
+    if request.method == "POST":
+        new_rating = int(request.POST.get("rating", 0))
+        product = get_object_or_404(Product, id=id)
+        match new_rating:
+            case 1:
+                new_rating = 5
+            case 2:
+                new_rating = 4
+            case 4:
+                new_rating = 2
+            case 5:
+                new_rating = 1
+       
+             
+        if 1 <= new_rating <= 5:
+            product.update_rating(new_rating)
+            print(new_rating)
+            return redirect('productPage',id=id)
+    return render(request, 'product-page.html', {'product': product})    
+    
