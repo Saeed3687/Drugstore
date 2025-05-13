30571859de4bb3307eb7ce50154bb3bd9bbad40f
Qasim Yusofi <qasem.yousifi20@gmail.com>
2025-05-13 21:37:11 +0330

    debugged images of categories

--- mainPage/models.py
+++ mainPage/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    image = models.ImageField(upload_to='static/images/categories/', null=True, blank=True)
 
     def __str__(self):
         return self.name
